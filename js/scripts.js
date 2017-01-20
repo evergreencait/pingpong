b5566efeecf083a2077a7383f2a41f0623adf138
Caitlin Hines <caitlinhines@me.com>
2017-01-20 09:28:21 -0800

    working function for counting up to number. Count starts with 10

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,30 @@
+//business logic
+  var pingPong = function(count) {
+    if (count) {
+      var answer = 1;
+      for (var number = 0; number <= count; number+=1) {
+        answer += number + ",";
+      }
+      return answer;
+    }
+  };
+
+
+
+
+
+
+
+
+
+
 //front end logic
 $(document).ready(function(){
   $("form#pingpong").submit(function(event){
     event.preventDefault();
     var numberInput = parseInt($("input#number").val());
-    var result = pingPong(input);
-    $("#result").text(result);
+    var result = pingPong(numberInput);
 
-  }):
+    $(".result").text(pingPong(numberInput));
+  });
 });
