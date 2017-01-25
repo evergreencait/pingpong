f8ee068ce545201095354ce65dd07c57854d2caa
Caitlin Hines <caitlinhines@me.com>
2017-01-24 19:46:50 -0800

    functioning for loops to result in ping, pong, and ping-pong

--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,21 @@
 //business logic
   var pingPong = function(numberInput) {
-
-  // var countBy = function(numberInput) {
-    if (numberInput) {
-      var answer = "";
+     var resultArray = [];
       for (var number = 1; number <= numberInput; number+=1) {
-        answer += number + ", ";
+        if (number % 15 === 0) {
+          resultArray.push("ping-pong");
       }
-      return answer
+       else if (number % 5 === 0) {
+        resultArray.push("pong");
+      }
+       else if (number % 3 === 0) {
+        resultArray.push("ping");
+      } else {
+        resultArray.push(number);
+      }
     }
-  // }
- //  var divisibleBy = function(numberInput) {
- //   var resultArray = [];
- //
- //   numberInput.forEach(function(number) {
- //     if (number % 3 === 0){
- //       resultArray.push("ping");
- //     } else if (number % 5 === 0) {
- //       resultArray.push("pong");
- //     } else if (number % 15 === 0) {
- //       resultArray.push("ping-pong")
- //     }
- //
- //   });
- //  };
- // });
- //
- //   var resultString = resultArray.join("");
- //   return resultString;
-
-
-
-//     var resultArray = [];
-//
-//     for (function(numberInput) {
-//     if (number % 3 === 0) {
-//       resultArray.push("ping");
-//     }
-//     if (number % 5 === 0) {
-//       resultArray.push("pong");
-//     }
-//     if (number % 15 === 0) {
-//         resultArray.push("ping-pong")
-//     }
-//     else {
-//         resultArray.push(answer);
-//     }
-//     var resultString = resultArray.join("");
-//     return resultString;
-// };
-};
-
-
-
+    return resultArray
+  };
 
 
 //front end logic
@@ -60,9 +23,11 @@ $(document).ready(function(){
   $("form#pingpong").submit(function(event){
     event.preventDefault();
     var numberInput = parseInt($("input#number").val());
-    var result = pingPong(numberInput);
+    var finalresultList = $(".result").text(pingPong(numberInput));
 
-    $(".result").text(pingPong(numberInput));
+
+    $("ul#resultsList").append("<li>" + finalresultList + "</li>");
+
 
 
   });
